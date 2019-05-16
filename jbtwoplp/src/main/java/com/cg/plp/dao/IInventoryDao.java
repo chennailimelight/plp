90059d885b72cacae2ed481991c2b9fbc671d89a
Rishabh Sahani <rishabhsahani1996@gmail.com>
2019-05-16 19:29:13 +0530

    4

--- jbtwoplp/src/main/java/com/cg/plp/dao/IInventoryDao.java
+++
@@ -1,9 +0,0 @@
-package com.cg.plp.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.CustomerProducts.model.Inventory;
-
-public interface IInventoryDao extends JpaRepository<Inventory, Integer> {
-
-}
