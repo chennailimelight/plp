61c122266b8804a719ad5c4ce93ced6ce08a6a04
A S Pradeep <pradeepjackson15@gmail.com>
2019-05-17 11:53:15 +0530

    Pradeep added Email service

---
+++ jbtwoplp/src/main/java/com/cg/plp/SendMailSSL.java
@@ -0,0 +1,15 @@
+package com.cg.plp;
+
+
+public class SendMailSSL{    
+ public static void main(String[] args) {    
+     //from,password,to,subject,message  
+     	Mailer.send("pradeepjackson15@gmail.com","password","souravgoyal572@gmail.com","testing","its working");  
+     //change from, password and to  
+ }    
+} 
+
+
+
+
+
---
+++ jbtwoplp/src/main/java/com/cg/plp/model/mail.java
@@ -0,0 +1,36 @@
+package com.cg.plp.model;
+
+import java.util.Properties;    
+import javax.mail.*;    
+import javax.mail.internet.*;    
+class Mailer {  
+    public static void send(String from,String password,String to,String sub,String msg){  
+          //Get properties object    
+          Properties props = new Properties();    
+          props.put("mail.smtp.host", "smtp.gmail.com");    
+          props.put("mail.smtp.socketFactory.port", "587");    
+          props.put("mail.smtp.socketFactory.class",    
+                    "javax.net.ssl.SSLSocketFactory");    
+          props.put("mail.smtp.auth", "true");    
+          props.put("mail.smtp.port", "587"); 
+          props.put("mail.smtp.starttls.enable", "true");
+          //get Session   
+          Session session = Session.getDefaultInstance(props,    
+           new javax.mail.Authenticator() {    
+           protected PasswordAuthentication getPasswordAuthentication() {    
+           return new PasswordAuthentication(from,password);  
+           }    
+          });    
+          //compose message    
+          try {    
+           MimeMessage message = new MimeMessage(session);    
+           message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));    
+           message.setSubject(sub);    
+           message.setText(msg);    
+           //send message  
+           Transport.send(message);    
+           System.out.println("message sent successfully");    
+          } catch (MessagingException e) {throw new RuntimeException(e);}    
+             
+    }  
+}  
