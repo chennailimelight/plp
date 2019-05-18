dfd4f028bb94f2f32affa5010584027c4170eea1
neila1997 <abraham.neil1997@gmail.com>
2019-05-18 11:42:13 +0530

    Feedback Method
    New method to send feedback to either Customer/Merchant

--- jbtwoplp/src/main/java/com/cg/plp/service/IAdminService.java
+++ jbtwoplp/src/main/java/com/cg/plp/service/IAdminService.java
@@ -1,6 +1,5 @@
 package com.cg.plp.service;
 
-
 import java.util.List;
 
 import com.cg.plp.model.Customer;
@@ -8,14 +7,19 @@ import com.cg.plp.model.Inventory;
 import com.cg.plp.model.Merchant;
 
 public interface IAdminService {
-	
-public String generateCoupon();
-public List<Customer> viewCustomer();
-public List<Merchant> viewMerchant();
-public List<Inventory> viewInventory();
 
-public void sendEmail(String receiverEmail,String subject,String message);
-public void sendCoupon(String receiverEmail);
-public void sendFeedback(Feedback feedback);
+	public String generateCoupon();
+
+	public List<Customer> viewCustomer();
+
+	public List<Merchant> viewMerchant();
+
+	public List<Inventory> viewInventory();
+
+	public void sendEmail(String receiverEmail, String subject, String message);
+
+	public void sendCoupon(String receiverEmail);
+
+	public void sendFeedback(Feedback feedback);
 
 }
--- jbtwoplp/src/main/java/com/cg/plp/service/IAdminServiceImpl.java
+++ jbtwoplp/src/main/java/com/cg/plp/service/IAdminServiceImpl.java
@@ -1,6 +1,5 @@
 package com.cg.plp.service;
 
-
 import java.util.List;
 import java.util.Properties;
 
@@ -31,19 +30,17 @@ public class IAdminServiceImpl implements IAdminService {
 	IInventoryDao inventory;
 	@Autowired
 	CustomerDao customer;
-	
+
 	@Override
 	public String generateCoupon() {
-		int length=10;
-		String coupon="";
-		String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                +"0123456789"
-                +"abcdefghijklmnopqrstuvxyz"; 
-		for(int i=0;i<length;i++) {
-			int index=(int)((AlphaNumericString.length())*Math.random());
-			coupon+=AlphaNumericString.charAt(index);
+		int length = 10;
+		String coupon = "";
+		String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
+		for (int i = 0; i < length; i++) {
+			int index = (int) ((AlphaNumericString.length()) * Math.random());
+			coupon += AlphaNumericString.charAt(index);
 		}
-        return coupon;  
+		return coupon;
 	}
 
 	@Override
@@ -59,49 +56,49 @@ public class IAdminServiceImpl implements IAdminService {
 	@Override
 	public List<Inventory> viewInventory() {
 		return inventory.findAll();
-	}    
-	    public void sendEmail(String receiverEmail,String emailSubject,String emailMessage){  
-	          Properties props = new Properties();    
-	          props.put("mail.smtp.host", "smtp.gmail.com");    
-	          props.put("mail.smtp.socketFactory.port", "465");    
-	          props.put("mail.smtp.socketFactory.class",    
-	                    "javax.net.ssl.SSLSocketFactory");    
-	          props.put("mail.smtp.auth", "true");    
-	          props.put("mail.smtp.port", "465"); 
-	          props.put("mail.smtp.starttls.enable", "true");
-	          //get Session   
-	          Session session = Session.getDefaultInstance(props,    
-	           new javax.mail.Authenticator() {    
-	           protected PasswordAuthentication getPasswordAuthentication() {    
-	           return new PasswordAuthentication("capgeministore9@gmail.com","capstore123!");  
-	           }    
-	          });      
-	          try {    
-	           MimeMessage message = new MimeMessage(session);    
-	           message.addRecipient(Message.RecipientType.TO,new InternetAddress(receiverEmail));
-	            message.setSubject(emailSubject);    
-	           message.setText(emailMessage);  
-	           Transport.send(message);    
-	           System.out.println("message sent successfully");    
-	          } catch (MessagingException e) {
-	        	  e.printStackTrace();
-	          }    
-	    }
+	}
 
-
-		@Override
-		public void sendFeedback(Feedback feedback) {
-			this.sendEmail(feedback.getEmail(), feedback.getSubject(), feedback.getMessage);
+	public void sendEmail(String receiverEmail, String emailSubject, String emailMessage) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+		props.put("mail.smtp.starttls.enable", "true");
+		// get Session
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("capgeministore9@gmail.com", "capstore123!");
+			}
+		});
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiverEmail));
+			message.setSubject(emailSubject);
+			message.setText(emailMessage);
+			Transport.send(message);
+			System.out.println("message sent successfully");
+		} catch (MessagingException e) {
+			e.printStackTrace();
 		}
+	}
 
-		@Override
-		public void sendCoupon(String receiverEmail) {
-			String subject ="Welcome to CapStore. A special coupon has been added to your account.";
-			String coupon=this.generateCoupon();
-			String userName="";
-	        String message ="Dear "+userName+"\n\n Welcome to the CapStore as a token of appreciation we are giving you a special discount. Avail an extra 35% offer at Checkout.\nUse Coupon Code: "+coupon;
-	        this.sendEmail(receiverEmail, subject, message);
-		          
-		}
-	    
-	}  
+	@Override
+	public void sendFeedback(Feedback feedback) {
+		this.sendEmail(feedback.getEmail(), feedback.getSubject(), feedback.getMessage);
+	}
+
+	@Override
+	public void sendCoupon(String receiverEmail) {
+		String subject = "Welcome to CapStore. A special coupon has been added to your account.";
+		String coupon = this.generateCoupon();
+		String userName = "";
+		String message = "Dear " + userName
+				+ "\n\n Welcome to the CapStore as a token of appreciation we are giving you a special discount. Avail an extra 35% offer at Checkout.\nUse Coupon Code: "
+				+ coupon;
+		this.sendEmail(receiverEmail, subject, message);
+
+	}
+
+}
