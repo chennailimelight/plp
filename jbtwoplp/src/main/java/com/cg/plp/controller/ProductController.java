abf2edacf5d6aed4c5d72081235d3971de9634da
Sumitava Aich <sumitava97@gmail.com>
2019-05-20 11:03:25 +0530

    commit

--- jbtwoplp/src/main/java/com/cg/plp/controller/ProductController.java
+++ jbtwoplp/src/main/java/com/cg/plp/controller/ProductController.java
@@ -1,16 +1,14 @@
-package com.cg.product.Controller;
+package com.cg.plp.controller;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cg.product.Product;
-import com.cg.product.dao.ProductDao;
-import com.cg.product.Service.*;
+import com.cg.plp.model.Product;
+import com.cg.plp.service.ProductService;
 
 @RestController
 /* @CrossOrigin(origins = "http://localhost:4200") */
